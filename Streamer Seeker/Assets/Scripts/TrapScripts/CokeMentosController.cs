feb3b424897d5352803d9f7986622c4bcdf77cee
TommyOhara <33668237+TommyOhara@users.noreply.github.com>
2019-01-26 16:51:55 -0500

    Added scripts for placing traps and basic trap scripts

---
+++ Streamer Seeker/Assets/Scripts/TrapScripts/CokeMentosController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CokeMentosController : MonoBehaviour
+{
+    public GameObject cm;
+    private List<GameObject> targets;
+
+    public void Start()
+    {
+        this.targets = new List<GameObject>();
+        Physics.queriesHitTriggers = true;
+    }
+
+    public void MakeActive()
+    {
+        this.cm.SetActive(true);
+    }
+
+    public void OnMouseDown()
+    {
+        Blast();
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Viewer") && !targets.Contains(collision.GetComponent<GameObject>()))
+        {
+            this.targets.Add(collision.GetComponent<GameObject>());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Viewer") && targets.Contains(collision.GetComponent<GameObject>()))
+        {
+            this.targets.Remove(collision.GetComponent<GameObject>());
+        }
+    }
+
+    public void Blast()
+    {
+        //get the viewer data for each game object in targets and do what needs to be done.
+        this.targets.Clear();
+        Destroy(this);
+    }
+}
