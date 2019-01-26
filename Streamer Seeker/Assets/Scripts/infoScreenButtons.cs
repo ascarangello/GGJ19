e3b57c2d5a8103fd5b4104fa8226cb67b4fe6c84
ascarangello <scarangello.a@husky.neu.edu>
2019-01-26 14:29:54 -0500

    added twitch info menu, flows from main to info to chat test atm

---
+++ Streamer Seeker/Assets/Scripts/infoScreenButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class infoScreenButtons : MonoBehaviour
+{
+    public InputField usernameF, channelNameF, passwordF;
+    public Button submitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        submitButton.onClick.AddListener(saveFields);
+    }
+
+    void saveFields()
+    {
+        dataSaver.Instance.username = usernameF.text;
+        dataSaver.Instance.channelName = channelNameF.text;
+        dataSaver.Instance.password = passwordF.text;
+        SceneManager.LoadScene("ChatTest");
+    }
+}
