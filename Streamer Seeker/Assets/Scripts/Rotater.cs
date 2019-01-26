cb189b5b153e812804ebbbad3bece4e4f0f1bea0
book81able <ecobb81@gmail.com>
2019-01-26 18:24:08 -0500

    Help, I need somebody help

---
+++ Streamer Seeker/Assets/Scripts/Rotater.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+
+	void Update()
+	{
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+	}
+	void FixedUpdate()
+    {
+        if (vertical > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (vertical < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+        else if (horizontal > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+        else if (horizontal < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+    }
+}
