9abf627f4c1aa6e8b57cf2c5c9538f21eb30ff48
jcameraa <camera.j@husky.neu.edu>
2019-01-26 20:21:06 -0500

    hahahahaha

--- Streamer Seeker/Assets/Scripts/spawnEnemy.cs
+++ Streamer Seeker/Assets/Scripts/spawnEnemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class spawnEnemy : MonoBehaviour
 {
+
+    public GameObject viewer;
+    public float spawnTime = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
